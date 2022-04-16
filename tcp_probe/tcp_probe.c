e0732b9255fb6bc0b422faf78adc839efb159992
Park JoonSoo <42.4.joonpark@gmail.com>
2022-04-16 12:00:59 +0900

    Update tcp_probe.c

--- tcp_probe/tcp_probe.c
+++ tcp_probe/tcp_probe.c
@@ -1,7 +1,7 @@
 #include "vmlinux.h"
 #include "bpf_helpers.h"
 
-char __license[] SEC("license") = "Dual MIT/GPL";
+char __license[] SEC("license") = "GPL";
 
 struct bpf_map_def SEC("maps") events = {
 	.type        = BPF_MAP_TYPE_ARRAY,
